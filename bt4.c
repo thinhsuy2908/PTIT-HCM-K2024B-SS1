7bff64042664f629eb81a5d2ce16ed2ca0608c95
thinhsuy2908 <phan90539@gmail.com>
2024-12-10 23:19:26 +0700

    bt4

---
+++ bt4.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("nhap gia tri bien a");
+    scanf("%f",& a);
+    printf("%.2f x %.2f=%.2f\n",a,a,a*a);
+    printf("%.2f x 4=%.2f\n",a,a*4);
+
+
+    return 0;
+}

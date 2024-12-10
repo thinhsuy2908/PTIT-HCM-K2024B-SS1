63ef4577cc02296555eb41d518d234f737483c56
thinhsuy2908 <phan90539@gmail.com>
2024-12-10 23:22:34 +0700

    bt1

---
+++ bt1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+
+//int la bien de luu tru so nguyen
+int age = 18;
+printf("Toi %d tuoi \n", age);
+
+//float la bien de luu tru so thap phan
+float height = 55.5;
+printf("Toi nang %.1f ky \n", height);
+
+//double giong nhu float nhung chinh xac hon
+double toado = 22.2222;
+printf("Toa do nha toi la %.4f \n", toado);
+
+//char bien de luu tru ky tu
+char text = 'a';
+printf("Ky tu la %c \n", text);
+
+
+
+
+	return 0;
+}
+
+
+

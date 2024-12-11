b1f45f55af31b512259436eda15f2ef655e8eb70
thinhsuy2908 <phan90539@gmail.com>
2024-12-11 10:23:34 +0700

    bt6ss3

---
+++ bt6ss3.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float a,t;
+    printf("nhap canh tam giac");
+    scanf("%f",&a);
+    printf("nhap chieu cao tam giac");
+    scanf("%f",&t);
+    printf("dien tich tam giac là: %.2f\n",(a*t)/2);
+   
+
+
+    return 0;
+}

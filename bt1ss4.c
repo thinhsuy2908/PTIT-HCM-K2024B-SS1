f3aa18fb87362a796629f04f14b5e214f224bf7b
thinhsuy2908 <phan90539@gmail.com>
2024-12-12 18:36:12 +0700

    bt1ss4

---
+++ bt1ss4.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float n;
+    printf("nhap 1 so nguyen");
+    scanf("%f",&n);
+    if(n>0){
+    printf("la so duong");
+    }else{ 
+    printf("la so am");
+    }
+    return 0;
+}

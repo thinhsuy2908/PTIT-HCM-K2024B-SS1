423df215e094a91cf58bc939704f28f140978bc4
thinhsuy2908 <phan90539@gmail.com>
2024-12-22 17:12:07 +0700

    bt1lap

---
+++ bt1lap.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    for(int i=100;0<i;i--)
+    printf("100 -1 = %d\n",i);
+
+    return 0;
+}

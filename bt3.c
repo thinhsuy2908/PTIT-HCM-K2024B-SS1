9b4a747c8233d70db1c889068f1abcd4a5e564d9
thinhsuy2908 <phan90539@gmail.com>
2024-12-29 17:29:37 +0700

    bt3ls9

--- bt3.c
+++ bt3.c
@@ -1,26 +1,31 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <stdio.h>
-
-int main()
-{
-    float a,b;
-    printf("nhap gia tri bien a");
-    scanf("%f",& a);
-    printf("nhap gia tri bien b");
-    scanf("%f",& b);
-    printf("a = %f va b = %f \n",a,b);
-    printf("%.2f + %.2f=%.2f\n",a,b,a+b);
-    printf("%.2f - %.2f=%.2f\n",a,b,a-b);
-    printf("%.2f x %.2f=%.2f\n",a,b,a*b);
-    printf("%.2f : %.2f=%.2f\n",a,b,a/b);
-
-
-    return 0;
+#include<stdio.h>
+int main(){
+	int n, currentLength,index;
+	const int maxSize=100;
+	int array[maxSize];
+	printf("moi nhap so phan tu muon su dung: ");
+	scanf("%d",&n);
+	printf("nhap vao gia tri phan tu: \n");
+	for(int i=0;i<n;i++){
+		printf("array[%d] = ",i);
+		scanf("%d",&array[i]);
+	}
+	currentLength=n;
+	printf("moi nhap vao vi tri muon xoa trong mang : ");
+	scanf("%d",&index);
+	if(index<0||index>maxSize){
+		printf("khong hop le");
+		
+	}
+	else{
+		for(int i=currentLength-1;i>=index;i--){
+			array[i-1]=array[i];
+		}
+		currentLength--;
+	}
+	printf("mang sau khi xoa la: \n");
+	for(int i=0;i<currentLength;i++){
+		printf("%d ",array[i]);
+		
+	}
 }

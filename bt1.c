ceda15f604e49fb58fa4a81e29abad5acb3000f5
thinhsuy2908 <phan90539@gmail.com>
2024-12-29 17:27:38 +0700

    bt1ls9

--- bt1.c
+++ bt1.c
@@ -1,27 +1,61 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-
-//int la bien de luu tru so nguyen
-int age = 18;
-printf("Toi %d tuoi \n", age);
-
-//float la bien de luu tru so thap phan
-float height = 55.5;
-printf("Toi nang %.1f ky \n", height);
-
-//double giong nhu float nhung chinh xac hon
-double toado = 22.2222;
-printf("Toa do nha toi la %.4f \n", toado);
-
-//char bien de luu tru ky tu
-char text = 'a';
-printf("Ky tu la %c \n", text);
-
-
-
-
-	return 0;
+	const int maxSize=100;
+	int array[maxSize];
+	int n, currentLength,addIndex,value;
+	printf("moi nhap vao so luong phan tu muon su dung : ");
+	scanf("%d",&n);
+	
+	
+	printf("nhap gia tri vao phan tu : \n");
+	for(int i=0;i<n;i++){
+		printf("array[%d] = ",i);
+		scanf("%d",&array[i]);
+	}
+	currentLength=n;
+	printf("moi nhap vao gia tri : ");
+	scanf("%d",&value);
+	printf("moi nhap vao vi tri muon them vao mang : ");
+	scanf("%d",&addIndex);
+	if(addIndex<0||addIndex>maxSize){
+		printf("khong hop le");
+	}
+	else if(addIndex>=currentLength){
+		array[addIndex]=value;
+		currentLength=addIndex+1;
+	}else {
+		for(int i=currentLength-1;i>=addIndex;i--){
+			array[i+1]=array[i];
+		}
+		array[addIndex]=value;
+		currentLength++;
+	}
+	printf("mang sau khi them la: \n");
+	for(int i=0;i<currentLength;i++){
+		printf("%d ",array[i]);
+		
+	}
+	
+	printf("\nMang hien tai: \n");
+	for(int i=0;i<currentLength;i++)
+	{
+		printf("%d ",array[i]);
+	}
+	printf("\nNhap vao vi tri can sua: ");
+	scanf("%d",&addIndex);
+	if(addIndex<0 || addIndex>currentLength)
+	{
+		printf("Vi tri khong hop le!");
+		return 1;
+	}
+	printf("Nhap vao gia tri moi: ");
+	scanf("%d",&value);
+	array[addIndex-1]=value;
+	printf("Mang sau khi sua: ");
+	for(int i=0;i<10;i++)
+	{
+		printf("%d ",array[i]);
+	}
+	
+return 0;
 }
-
-
-
